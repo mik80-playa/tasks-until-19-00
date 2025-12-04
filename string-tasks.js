6684c3fd171d5ebdd858f030f893760878544d4a
Fo18 <fo18@MacBook-Pro-Fo18.local>
2025-12-04 17:03:41 +0300

    Remove test file

--- string-tasks.js
+++ string-tasks.js
@@ -1,47 +1,39 @@
 // Задача 1
-function concatenateStrings(str1, str2) {
-  return (str1 + str2).replace(/ /g, '');
+const concatenateStrings = (str1, str2) => {
+  return (str1 + str2).replace(/\s+/g, '');
 }
 
 // Задача 2
-function getStringLength(str) {
+const getStringLength = (str) => {
   return str.length;
 }
 
 // Задача 3
-function getStringFromTemplate(firstName, lastName) {
+const getStringFromTemplate = (firstName, lastName) => {
   return `Hello, ${firstName} ${lastName}!`;
 }
 
 // Задача 4
-function getChar(str, index) {
+const getChar = (str, index) => {
   return str[index - 1];
 }
 
 // Задача 5
-function removeFirstOccurrences(str, substring) {
+const removeFirstOccurrences = (str, substring) => {
   return str.replace(substring, '');
 }
 
 // Задача 6
-function getRectangleString(width, height) {
-  let result = '';
-
-  for (let i = 0; i < height; i++) {
-    if (i === 0 || i === height - 1) {
-      // Первая и последняя строка - все звездочки
-      result += '*'.repeat(width);
-    } else {
-      // Средние строки - звездочка, пробелы, звездочка
-      result += '*' + ' '.repeat(width - 2) + '*';
-    }
-
-    // Добавляем перенос строки, кроме последней строки
-    if (i < height - 1) {
-      result += '\n';
-    }
+const getRectangleString = (width, height) => {
+  if (width < 2 || height < 2) return '*'.repeat(width);
+  const top = '*'.repeat(width);
+  const middle = '*' + ' '.repeat(width - 2) + '*';
+  const bottom = '*'.repeat(width);
+  let result = top + '\n';
+  for (let i = 0; i < height - 2; i++) {
+    result += middle + '\n';
   }
-
+  result += bottom;
   return result;
 }
 
